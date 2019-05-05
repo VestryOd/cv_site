71e769d0cbfa19365f24343689b7b6f2e3b6bcc5
Maksym Kaspriv <VestryOd@users.noreply.github.com>
2019-05-05 23:14:37 +0300

    adding first ptece of work

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+/*
+ * Third party
+ */
+//= ../bower_libs/jquery/dist/jquery.js
+
+
+/*
+ * Custom
+ */
+//= partials/_header.js
+//
+
+window.onload(function() {
+	// var newHeight = document.documentElement.clientHeight;
+
+	// main menu open/close
+	var mainMenu = document.getElementById("main_menu");
+	var btnMenuClose = document.getElementById("btn_menu_close");
+
+	
+})
